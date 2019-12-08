34915a68075f8e5d81e788badae1c31af2126a82
Brian Trang <btrang@casestack.com>
2019-12-07 21:56:32 -0800

    configurations for yaml, lambda func

--- lambda/brian-lambda.js
+++ lambda/brian-lambda.js
@@ -1,7 +1,23 @@
-exports.handler = async function(event, context) {
-    event.Records.forEach(record => {
-      const { body } = record;
-      console.log(body);
-    });
-    return {};
+const aws = require("aws-sdk")
+const s3 = new aws.S3();
+
+
+
+
+
+
+exports.handler = async function (event, context) {
+  console.log(JSON.stringify(event))
+  const params = {
+    Body: JSON.stringify(event),
+    Bucket: process.env.SOURCE_BUCKET,
+    Key: "toBeUpload.json",
+    ContentType: "application/json"
+  };
+  try {
+    const res = await s3.putObject(params).promise();
+    console.log('complete:', res);
+  } catch(err) {
+    throw new Error(err);
   }
+}
