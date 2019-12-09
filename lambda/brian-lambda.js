f6f7e3830a32a9381ae2a09b7a1d5e3c6a8a2ce1
Brian Trang <btrang@casestack.com>
2019-12-09 09:03:04 -0800

    policy for s3 to sqs

--- lambda/brian-lambda.js
+++ lambda/brian-lambda.js
@@ -1,5 +1,9 @@
 const s3_helper=require("../lib/aws/s3");
 
+
+//TODO:
+//s3 notification sent to sqs
+//add check for S3 and sqs
 exports.handler = async function (event, context) {
   console.log(JSON.stringify(event))
   await s3_helper.upload(event);
